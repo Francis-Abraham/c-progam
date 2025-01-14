02720e5e16938a64a831b8b0892df1c60e75a873
Francis-Abraham <franciscoabraham246@gmail.com>
2025-01-14 17:58:07 +0530

    Add files via upload

--- model Q/occurrence.c
+++ model Q/occurrence.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 
-void occurrence(int arr[], int size) {
+void occurrence(int arr[], int size)
+{
     int count, i, j;
     printf("Element\tOccurrences\n");
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++)
+    {
         count = 1;
-        for (j = i + 1; j < size; j++) {
-            if (arr[i] == arr[j]) {
+        for (j = i + 1; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
                 count++;
                 // Mark the counted element to avoid counting it again
-                arr[j] = -1; 
+                arr[j] = -1;
             }
         }
-        if (arr[i] != -1) {
+        if (arr[i] != -1)
+        {
             printf("%d\t%d\n", arr[i], count);
         }
     }
 }
 
-int main() {
+int main()
+{
     int n, i;
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
     int arr[n];
     printf("Enter the elements:\n");
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++)
+    {
         scanf("%d", &arr[i]);
     }
     occurrence(arr, n);
